4db6b19b54992ebf624d41f31699a99733c57aa1
fahdjamy <fahdjamy2@gmail.com>
2024-08-28 00:04:42 -0500

    08/27/2024
    - set up docker
    - change configurations and have operate configuration files for different environments.

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
+DOCKER_BUILD_TAG := zero2prod
+
 run-debug:
 	@echo "Starting the application in debug mode with detailed logging..."
 	RUST_LOG=debug cargo run
@@ -15,3 +17,23 @@ run-test-debug:
 	@echo "Running tests with test logs"
 	# all tests
 	TEST_LOG=true cargo test | bunyan
+
+docker-release-build:
+	# “Using . we are telling Docker to use the current directory as the build context for this image; COPY .
+	# app will therefore copy all files from the current directory (including our source code!)
+	# into the app directory of our Docker image.
+	# Using . as build context implies, for example,
+	# that Docker will not allow COPY to see files from the parent directory or
+	# from arbitrary paths on your machine into the image.
+	@echo "Building a docker release image for the app"
+	docker build --tag $(DOCKER_BUILD_TAG) --file Dockerfile .
+
+docker-run-release-build:
+	docker run -p 8001:8001 $(DOCKER_BUILD_TAG)
+
+prepare-sqlx:
+	# prepare performs the same work that is usually done when cargo build is invoked but it saves the outcome of
+	# those queries into a directory (.sqlx) which can later be detected by sqlx itself and used to skip the queries
+	# altogether and perform an offline build.
+	# this will force sqlx to look at the saved metadata instead of querying a live DB
+	cargo sqlx prepare --workspace
--- src/configuration.rs
+++ src/configuration.rs
@@ -38,6 +38,7 @@ impl DatabaseSettings {
 #[derive(serde::Deserialize)]
 pub struct ApplicationSettings {
     pub port: u16,
+    pub host: String,
 }
 
 #[derive(serde::Deserialize)]
@@ -47,16 +48,59 @@ pub struct Settings {
 }
 
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    let base_path = std::env::current_dir().expect("failed to determine current dir");
+
+    let configuration_dir = base_path.join("configuration");
+
+    // Detect the running environment.
+    // Default to `local` if unspecified.
+    let environment: Environment = std::env::var("APP_ENVIRONMENT")
+        .unwrap_or_else(|_| "local".into())
+        .try_into()
+        .expect("Failed to parse APP_ENVIRONMENT.");
+
+    let environment_filename = format!("{}.yaml", environment.as);
+
     // Initialise our configuration reader
     let settings = config::Config::builder()
         // Add configuration values from a file named `configuration.yaml`.
-        .add_source(config::File::new(
-            "configuration.yaml",
-            config::FileFormat::Yaml,
+        .add_source(config::File::from(configuration_dir.join("base.yaml")))
+        .add_source(config::File::from(
+            configuration_dir.join(environment_filename),
         ))
         .build()?;
 
     // Try to convert the configuration values it read into
     // our Settings type
     settings.try_deserialize::<Settings>()
+}
+
+/// The possible runtime environment for our application.
+pub enum Environment {
+    Local,
+    Production,
+}
+
+impl Environment {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Environment::Local => "local",
+            Environment::Production => "production",
+        }
+    }
+}
+
+impl TryFrom<String> for Environment {
+    type Error = String;
+
+    fn try_from(s: String) -> Result<Self, Self::Error> {
+        match s.to_lowercase().as_str() {
+            "local" => Ok(Self::Local),
+            "production" => Ok(Self::Production),
+            other => Err(format!(
+                "{} is not a supported environment. Use either `local` or `production`.",
+                other
+            )),
+        }
+    }
 }
