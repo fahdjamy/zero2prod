635c7f7e3baf3c9eaf951f218c69d9cb899f33e6
fahdjamy <fahdjamy2@gmail.com>
2024-09-25 23:51:04 -0500

    09/25/2024
    - add authentication logic bas64 handler.
    - add test for invalid authentication headers.

--- src/routes/newsletter.rs
+++ src/routes/newsletter.rs
@@ -1,9 +1,13 @@
 use crate::domain::SubscriberEmail;
 use crate::email_client::EmailClient;
 use crate::routes::error_chain_fmt;
-use actix_web::http::StatusCode;
-use actix_web::{HttpResponse, ResponseError};
+use actix_web::body::BoxBody;
+use actix_web::http::header::{HeaderMap, HeaderValue};
+use actix_web::http::{header, StatusCode};
+use actix_web::{HttpRequest, HttpResponse, ResponseError};
 use anyhow::Context;
+use base64::Engine;
+use secrecy::Secret;
 use sqlx::PgPool;
 
 struct ConfirmedSubscriber {
@@ -12,6 +16,8 @@ struct ConfirmedSubscriber {
 
 #[derive(thiserror::Error)]
 pub enum PublishError {
+    #[error("Authentication Failed")]
+    AuthError(#[source] anyhow::Error),
     #[error(transparent)]
     // anyhow::Error provides the capability to enrich an error with additional context out of the box.
     UnexpectedError(#[from] anyhow::Error),
@@ -24,9 +30,25 @@ impl std::fmt::Debug for PublishError {
 }
 
 impl ResponseError for PublishError {
-    fn status_code(&self) -> StatusCode {
+    // `status_code` is invoked by the default `error_response`
+    // implementation. We are providing a bespoke `error_response` implementation
+    // therefore there is no need to maintain a `status_code` implementation anymore.
+
+    fn error_response(&self) -> HttpResponse<BoxBody> {
         match self {
-            PublishError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            PublishError::AuthError(_) => {
+                let mut response = HttpResponse::new(StatusCode::UNAUTHORIZED);
+                let header_value = HeaderValue::from_str(r#"Basic realm="publish""#).unwrap();
+                response
+                    .headers_mut()
+                    // actix_web::http::header provides a collection of constants
+                    // for the names of several well-known/standard HTTP headers
+                    .insert(header::WWW_AUTHENTICATE, header_value);
+                response
+            }
+            PublishError::UnexpectedError(_) => {
+                HttpResponse::new(StatusCode::INTERNAL_SERVER_ERROR)
+            }
         }
     }
 }
@@ -43,11 +65,21 @@ pub struct Content {
     text: String,
 }
 
+pub struct Credentials {
+    username: String,
+    password: Secret<String>,
+}
+
 pub async fn publish_newsletter(
-    body: actix_web::web::Json<BodyData>,
+    request: HttpRequest,
     pool: actix_web::web::Data<PgPool>,
+    body: actix_web::web::Json<BodyData>,
     email_client: actix_web::web::Data<EmailClient>,
 ) -> Result<HttpResponse, PublishError> {
+    let _basic_credentials = basic_auth(request.headers())
+        // Bubble up the error, performing the necessary conversion
+        .map_err(|e| PublishError::AuthError(e))?;
+
     let subscribers = get_confirmed_subscriber(&pool).await?;
 
     for subscriber in subscribers {
@@ -72,6 +104,9 @@ pub async fn publish_newsletter(
                 tracing::warn!(
                     // We record the error chain as a structured field
                     // on the log record.
+                    // ? before a value within a formatting macro, it instructs the macro to use the
+                    // Debug formatting trait for that value
+                    // https://stackoverflow.com/questions/74008676/question-mark-operator-before-variables-what-do-they-do-in-rust
                     error.cause_chain = ?err,
                     "Skipping a confirmed subscriber. Their stored contact details are invalid",
                 );
@@ -120,4 +155,40 @@ async fn get_confirmed_subscriber(
         .collect();
 
     Ok(confirmed_subscribers)
+}
+
+fn basic_auth(headers: &HeaderMap) -> Result<Credentials, anyhow::Error> {
+    // The header value, if present, must be a valid UTF8 string
+    let header_value = headers
+        .get("Authorization")
+        .context("The 'Authorization' header was missing")?
+        .to_str()
+        .context("The 'Authorization' header was not a valid UTF8 string.")?;
+
+    let base64encoded_segment = header_value
+        .strip_prefix("Basic ")
+        .context("The authorization scheme was not 'Basic'.")?;
+    let decoded_bytes = base64::engine::general_purpose::STANDARD
+        .decode(base64encoded_segment)
+        .context("Failed to base64-decode 'Basic' credentials.")?;
+    let decoded_credentials = String::from_utf8(decoded_bytes)
+        .context("The decoded credential string is not valid UTF8.")?;
+
+    // Split into two segments, using ':' as delimiter
+    let mut credentials = decoded_credentials.splitn(2, ':');
+
+    let username = credentials
+        .next()
+        .ok_or_else(|| anyhow::anyhow!("A username must be provided in 'Basic' auth."))?
+        .to_string();
+
+    let password = credentials
+        .next()
+        .ok_or_else(|| anyhow::anyhow!("A password must be provided in 'Basic' auth."))?
+        .to_string();
+
+    Ok(Credentials {
+        username,
+        password: Secret::new(password),
+    })
 }
