2683ce06771eb32f5356d3e3591a0c2749a3d41c
fahdjamy <fahdjamy2@gmail.com>
2024-10-17 23:06:11 -0500

    Mask timing attacks by introducing a default expected_password_hash.

--- src/routes/newsletter.rs
+++ src/routes/newsletter.rs
@@ -230,13 +230,26 @@ async fn validate_credentials(
     credentials: Credentials,
     pg_pool: &PgPool,
 ) -> Result<uuid::Uuid, PublishError> {
-    let (user_id, db_password_hash) = get_stored_credentials(&credentials.username, &pg_pool)
-        .await
-        .map_err(PublishError::UnexpectedError)?
-        .ok_or_else(|| PublishError::AuthError(anyhow::anyhow!("Unknown Username")))?;
+    // for preventing timing attacks
+    let mut user_id = None;
+    let mut expected_password_hash = Secret::new(
+        "$argon2id$v=19$m=15000,t=2,p=1$\
+        gZiV/M1gPc22ElAH/Jh1Hw$\
+        CWOrkoo7oJBQ/iyh7uJ0LO2aLEfrHwTWllSAxT0zRno"
+            .to_string(),
+    );
+
+    if let Some((stored_user_id, stored_password_hash)) =
+        get_stored_credentials(&credentials.username, &pg_pool)
+            .await
+            .map_err(PublishError::UnexpectedError)?
+    {
+        user_id = Some(stored_user_id);
+        expected_password_hash = stored_password_hash;
+    };
 
     spawn_blocking_with_tracing(move || {
-        verify_password_hash(db_password_hash, credentials.password)
+        verify_password_hash(expected_password_hash, credentials.password)
     })
     .await
     // spawn_blocking is fallible - we have a nested Result here!
@@ -245,7 +258,10 @@ async fn validate_credentials(
     .context("Invalid password")
     .map_err(PublishError::AuthError)?;
 
-    Ok(user_id)
+    // This is only set to `Some` if we found credentials in the store
+    // Even if the default password ends up matching (somehow)
+    // with the provided password, we never authenticate a non-existing user
+    user_id.ok_or_else(|| PublishError::AuthError(anyhow::anyhow!("Unknown username.")))
 }
 
 #[tracing::instrument(
