e453505975e1dfec308632c0480fc1b18a66ffc6
fahdjamy <fahdjamy2@gmail.com>
2024-10-22 18:09:00 -0500

    Set cookies for error messages when login fails.

--- tests/api/login.rs
+++ tests/api/login.rs
@@ -11,7 +11,13 @@ async fn an_error_flash_message_is_set_on_login_failure() {
     });
     let response = app.post_login(&login_body).await;
 
+    let flash_cookie = response
+        .cookies()
+        .find(|cookie| cookie.name() == "_flash")
+        .unwrap();
+
     // Assert
+    assert_eq!(flash_cookie.value(), "Authentication failed");
     assert_is_redirect_to(response, "/login");
 }
 
