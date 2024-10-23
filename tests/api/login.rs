b4746cd397e8054cf5ec5c61c270ab8c555468ac
fahdjamy <fahdjamy2@gmail.com>
2024-10-22 23:56:43 -0500

    Remove cookie once use has navigated to the login page after log failure

--- tests/api/login.rs
+++ tests/api/login.rs
@@ -17,11 +17,17 @@ async fn an_error_flash_message_is_set_on_login_failure() {
         .unwrap();
 
     // Assert
+    // Step 1; Try to log in
     assert_eq!(flash_cookie.value(), "Authentication failed");
     assert_is_redirect_to(response, "/login");
 
+    // Step 2; Follow the redirect and check that error is set
     let html_page = app.get_login_html().await;
     assert!(html_page.contains(r#"<p><i>Authentication failed</i></p>"#));
+
+    let html_page = app.get_login_html().await;
+    // Step 3; Reload log in page and error shouldn't be there anymore
+    assert!(!html_page.contains(r#"<p><i>Authentication failed</i></p>"#));
 }
 
 pub fn assert_is_redirect_to(response: reqwest::Response, location: &str) {
